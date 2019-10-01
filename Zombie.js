e1a183882946097035c8591aabb2dfa4f0cb53ed
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-10-01 13:09:08 -0400

    define Zombie.collect()

--- Zombie.js
+++ Zombie.js
@@ -4,4 +4,10 @@ class Zombie extends Sprite {
      this.SPEED = 2
     this.vx = -this.SPEED
   }
+  
+  collect(item) {
+    if ( item.active && checkCC(this.x, this.y, this.width, item.x, item.y, item.width) ) {
+        item.die()
+    }
+  }
 }
